242136c9a3d44771a9939bfb8074b4dacd5950b3
belgee7 <belge0722@gmail.com>
2024-10-09 09:57:20 +0800

    aa

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import { Typography } from "@mui/material";
+
 export default function Home() {
   return (
     <>
-    Hi
-    </>  );
-
+      <Typography color="error">Temdeg orsn baih ystoi</Typography>
+    </>
+  );
 }
