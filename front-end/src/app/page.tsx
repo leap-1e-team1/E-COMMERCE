5cebf7c730542e7e699e2da828f3c9503b4dadab
99039721 <96558484+99039721@users.noreply.github.com>
2024-10-09 12:34:43 +0800

    header (#33)
    Co-authored-by: 24lp3471@nest.edu.mn <24LP3471@G304-CC0117.local>

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { Header } from "@/components/Header";
 import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <Typography color="error">Temdeg orsn baih ystoi</Typography>
     </>
   );
---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+export const Header = () => {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>Header</Box>
+    </div>
+  );
+};
