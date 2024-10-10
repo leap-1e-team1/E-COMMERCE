73d775b9786ec2b0749169142811c4f28493e8fa
24LP4086 24LP4086 <24LP4086@G302-CC0115.local>
2024-10-10 12:01:43 +0800

    footer

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { Header } from "@/components/Header";
 import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Typography color="error">Temdeg orsn baih ystoi</Typography>
     </>
   );
