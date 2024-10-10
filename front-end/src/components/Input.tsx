d6199e0151503368a50e9df5be86788d2a5d1192
24LU1697 24LU1697 <24LU1697@G302-CC0111.local>
2024-10-10 10:46:37 +0800

    signup

--- front-end/src/components/Input.tsx
+++ front-end/src/components/Input.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import { TextField } from "@mui/material";
+import { colors, TextField } from "@mui/material";
 
 type InputProps = {
   label: string;
   placeholder: string;
+  helperText: string;
+  inputHandler: () => void;
 };
 
-export const Input = ({ label, placeholder }: InputProps) => {
+export const Input = ({
+  label,
+  placeholder,
+  helperText,
+  inputHandler,
+}: InputProps) => {
   return (
     <TextField
       label={label}
       variant="outlined"
       //   sx={{ py: 2, px: 3, height: "36px" }}
       placeholder={placeholder}
+      helperText={helperText}
+      onChange={inputHandler}
     />
   );
 };
