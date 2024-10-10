d69ef064a2ac5986bd801ab2c0b4146bdaf95ca3
shagai-rgb <dash.altanshagai48@gmail.com>
2024-10-10 11:13:15 +0800

    30 fe signup (#39)
    * signup
    
    * signup and input
    
    * s
    
    ---------
    
    Co-authored-by: 24LU1697 24LU1697 <24LU1697@G302-CC0111.local>

--- front-end/src/app/register/page.tsx
+++ front-end/src/app/register/page.tsx
@@ -2,7 +2,7 @@
 
 import { CustomButton } from "@/components/Button";
 import { Input } from "@/components/Input";
-import { Stack, Typography } from "@mui/material";
+import { List, ListItem, Stack, Typography } from "@mui/material";
 
 const Register = () => {
   return (
@@ -16,12 +16,106 @@ const Register = () => {
         Бүртгүүлэх
       </Typography>
       <Stack gap={4} sx={{ width: "334px" }}>
-        <Input label="Нэр" placeholder="Нэр" />
-        <Input label="Имэйл хаяг" placeholder="Имэйл хаяг" />
-        <Input label="Нууц үг" placeholder="Нууц үг" />
-        <Input label="Нууц үг давтах " placeholder="Нууц үг давтах " />
+        <Input
+          label="Нэр"
+          placeholder=""
+          helperText=""
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          placeholder=""
+          label="Имэйл хаяг"
+          helperText="Зөв имэйл хаяг оруулна уу"
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          label="Нууц үг"
+          placeholder=""
+          helperText="Нууц үг ижил биш байна"
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          label="Нууц үг давтах "
+          placeholder=""
+          helperText=""
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <List disablePadding sx={{ listStyleType: "disc", marginLeft: "20px" }}>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Том үсэг орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Жижиг үсэг орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Тоо орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Тэмдэгт орсон байх
+          </ListItem>
+        </List>
         <CustomButton
-          text="asd"
+          text="Үүсгэх"
+          bgColor=""
+          hoverColor=""
+          height=""
+          border=""
+          textColor=""
+          handleClick={() => {
+            console.log("Asd");
+          }}
+        />
+        <CustomButton
+          text="Нэвтрэх"
+          bgColor=""
+          hoverColor=""
+          height=""
+          border=""
+          textColor=""
           handleClick={() => {
             console.log("Asd");
           }}
