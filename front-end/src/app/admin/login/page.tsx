a8ac40e28b7a3bb04e05f5f28aeb11f911dbc85f
24lp3471@nest.edu.mn <24LP3471@G304-CC0117.local>
2024-10-16 12:36:24 +0800

    admin ehlev

---
+++ front-end/src/app/admin/login/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { CustomButton } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <Stack
+      gap={12}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        mt: "108px",
+      }}
+    >
+      <Typography color="primary.main" sx={{ fontSize: "24px" }}>
+        Нэвтрэх
+      </Typography>
+
+      <Stack
+        gap={4}
+        sx={{ width: "334px", display: "flex", justifyContent: "center" }}
+      >
+        <Input
+          label="Нэр"
+          placeholder="Нэр"
+          helperText=""
+          inputHandler={() => {
+            console.log();
+          }}
+        />
+        <Input
+          label="Нууц үг"
+          placeholder="Нууц үг"
+          helperText=""
+          inputHandler={() => {
+            console.log();
+          }}
+        />
+        <CustomButton
+          text="Нэвтрэх"
+          textColor="background.paper"
+          handleClick={() => console.log("Button clicked!")}
+          bgColor="secondary.main"
+          hoverColor="primary.main"
+          height="36px"
+          border="secondary.main"
+        />
+        <Link href="./register">
+          <Typography
+            component="div"
+            color="text.secondary"
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            Нууц үг мартсан
+          </Typography>
+        </Link>
+      </Stack>
+
+      <Stack sx={{ width: "334px" }}>
+        <CustomButton
+          text="Бүртгүүлэх"
+          textColor="secondary.main"
+          handleClick={() => console.log("Button clicked!")}
+          bgColor="background.paper"
+          hoverColor="primary.main"
+          height="36px"
+          border="secondary.main"
+        />
+      </Stack>
+    </Stack>
+  );
+}
---
+++ front-end/src/app/admin/register/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { CustomButton } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { List, ListItem, Stack, Typography } from "@mui/material";
+
+const Register = () => {
+  return (
+    <Stack
+      alignItems={"center"}
+      justifyContent={"center"}
+      gap={"16px"}
+      mt={"100px"}
+      mb={"100px"}
+    >
+      <Typography color="text" fontSize={"24px"} fontWeight={"600"}>
+        Бүртгүүлэх
+      </Typography>
+      <Stack gap={4} sx={{ width: "334px" }}>
+        <Input
+          label="Нэр"
+          placeholder=""
+          helperText=""
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          placeholder=""
+          label="Имэйл хаяг"
+          helperText="Зөв имэйл хаяг оруулна уу"
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          label="Нууц үг"
+          placeholder=""
+          helperText="Нууц үг ижил биш байна"
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <Input
+          label="Нууц үг давтах "
+          placeholder=""
+          helperText=""
+          inputHandler={() => {
+            console.log("Asd");
+          }}
+        />
+        <List disablePadding sx={{ listStyleType: "disc", marginLeft: "20px" }}>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Том үсэг орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Жижиг үсэг орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Тоо орсон байх
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{
+              display: "list-item",
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "20px",
+              color: "#71717A",
+            }}
+          >
+            Тэмдэгт орсон байх
+          </ListItem>
+        </List>
+        <CustomButton
+          text="Нэвтрэх"
+          textColor="background.paper"
+          handleClick={() => console.log("Button clicked!")}
+          bgColor="secondary.main"
+          hoverColor="primary.main"
+          height="36px"
+          border="secondary.main"
+        />
+        <CustomButton
+          text="Нэвтрэх"
+          textColor="background.paper"
+          handleClick={() => console.log("Button clicked!")}
+          bgColor="secondary.main"
+          hoverColor="primary.main"
+          height="36px"
+          border="secondary.main"
+        />
+      </Stack>
+    </Stack>
+  );
+};
+export default Register;
