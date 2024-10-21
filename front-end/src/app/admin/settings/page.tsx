914822efda0742f69d7ad030d971dd18036a245d
24LU1697 24LU1697 <24LU1697@G302-CC0111.local>
2024-10-21 13:56:27 +0800

    Asd

--- front-end/src/app/admin/settings/page.tsx
+++ front-end/src/app/admin/settings/page.tsx
@@ -1,14 +1,18 @@
 import { AdminHeaderComp } from "@/components/AdminHeaderComp";
+import { DashboardComp } from "@/components/DashboardComp";
 import { LeftBar } from "@/components/LeftBar";
 import { SettingsComp } from "@/components/SettingsComp";
+import { Stack } from "@mui/material";
 import React from "react";
 
 const SettingsPage = () => {
   return (
     <div>
       <AdminHeaderComp />
-      <LeftBar bgColor="Тохиргоо" />
-      <SettingsComp />
+      <Stack flexDirection={"row"} sx={{ width: "100vw" }}>
+        <LeftBar bgColor="Тохиргоо" />
+        <SettingsComp />
+      </Stack>
     </div>
   );
 };
--- front-end/src/components/SettingsComp.tsx
+++ front-end/src/components/SettingsComp.tsx
@@ -1,5 +1,136 @@
-import React from "react";
+"use client";
+
+import { Box, Stack, ImageList, Typography, Button } from "@mui/material";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { CustomButton } from "./Button";
 
 export const SettingsComp = () => {
-  return <div>SettingsComp</div>;
+  return (
+    <Box bgcolor={"background.paper"} sx={{ width: "100%" }}>
+      <Stack
+        flexDirection={"row"}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingX: "25px",
+          marginTop: "5%",
+        }}
+      >
+        <Stack
+          bgcolor={"primary.contrastText"}
+          flexDirection={"column"}
+          sx={{
+            width: "729px",
+            borderRadius: "10px",
+            display: "flex",
+            paddingY: "16px",
+            paddingX: "24px",
+            ml: "15%",
+            justifyContent: "space-between",
+            gap: "20px",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold", fontSize: "18px" }}>
+            Тохиргоо
+          </Typography>
+          <Stack
+            direction={"row"}
+            sx={{
+              px: "12px",
+              py: "8px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              border: "solid",
+              borderWidth: "1px",
+              borderColor: "background.paper",
+              borderRadius: "8px",
+            }}
+          >
+            <Typography sx={{ fontSize: "16px" }}>Баннер зураг </Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                bgcolor: "background.paper",
+                py: "8px",
+                px: "16px",
+                fontSize: "14px",
+                fontWeight: "600",
+                height: "36px",
+                borderColor: "#D6D8DB",
+                borderRadius: "8px",
+              }}
+            >
+              солих
+            </Button>
+          </Stack>
+          <Stack
+            direction={"row"}
+            sx={{
+              px: "12px",
+              py: "8px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              border: "solid",
+              borderWidth: "1px",
+              borderColor: "background.paper",
+              borderRadius: "8px",
+            }}
+          >
+            <Typography sx={{ fontSize: "16px" }}>
+              Эхний бүтээгдэхүүнээ нэмнэ үү
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                bgcolor: "background.paper",
+                py: "8px",
+                px: "16px",
+                fontSize: "14px",
+                fontWeight: "600",
+                height: "36px",
+                borderColor: "#D6D8DB",
+                borderRadius: "8px",
+              }}
+            >
+              Бүтээгдэхүүн нэмэх
+            </Button>
+          </Stack>
+          <Stack
+            direction={"row"}
+            sx={{
+              px: "12px",
+              py: "8px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              border: "solid",
+              borderWidth: "1px",
+              borderColor: "background.paper",
+              borderRadius: "8px",
+            }}
+          >
+            <Typography sx={{ fontSize: "16px" }}>
+              Хүргэлтийг тохируулна уу{" "}
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                bgcolor: "background.paper",
+                py: "8px",
+                px: "16px",
+                fontSize: "14px",
+                fontWeight: "600",
+                height: "36px",
+                borderColor: "#D6D8DB",
+                borderRadius: "8px",
+              }}
+            >
+              Хүргэлт тохируулах
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 };
