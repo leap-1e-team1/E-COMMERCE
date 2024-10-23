f3a76e14c9118b80c89100e5ea09de20a97b80cb
belgee7 <belge0722@gmail.com>
2024-10-23 12:50:04 +0800

    resolve conflict

--- front-end/src/app/cart/page.tsx
+++ front-end/src/app/cart/page.tsx
@@ -1,5 +1,4 @@
 import Carts from "@/components/Carts";
-import Delivery from "@/components/DeliveryInformation";
 import Steps from "@/components/Step";
 import { Stack } from "@mui/material";
 
@@ -12,12 +11,11 @@ export default function Cart() {
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        height: "90vh",
+        height: "80vh",
       }}
     >
       <Steps />
-      {/* <Carts /> */}
-      <Delivery />
+      <Carts />
     </Stack>
   );
 }
