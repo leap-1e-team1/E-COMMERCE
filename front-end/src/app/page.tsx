a3e5f6b59e316d2cbdbc96307a116c9c0be5b90c
Zoloo <mac@macs-MacBook-Air.local>
2024-10-23 12:28:44 +0800

    ASD

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { toast, ToastContainer } from "react-toastify"; // Correct import for both
-import axios from "axios";
-import "react-toastify/dist/ReactToastify.css";
+import OTP from "@/components/OTP";
+import { Stack, Typography } from "@mui/material";
+import NewPAss from "../components/NewPass";
+import Loading from "../components/Loading";
 
 export default function Home() {
-  const sendEmail = async () => {
-    try {
-      await axios.get("http://localhost:8000");
-      toast.success("Successfully sent email", { toastId: "success1" });
-    } catch (err) {
-      toast.error("Something went wrong", { toastId: "error1" });
-    }
-  };
-
   return (
     <Stack sx={{ minHeight: "100vh" }}>
       <Typography className="mt-[150px]" color="error">
