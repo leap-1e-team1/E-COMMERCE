346c9792e02e4c7568b1f5ba5d345e8c945ed01b
belgee7 <belge0722@gmail.com>
2024-10-23 12:52:57 +0800

    res

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
-import OTP from "@/components/OTP";
-import { Stack, Typography } from "@mui/material";
-import NewPAss from "../components/NewPass";
-import Loading from "../components/Loading";
+import { toast, ToastContainer } from "react-toastify"; // Correct import for both
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const sendEmail = async () => {
+    try {
+      await axios.get("http://localhost:8000");
+      toast.success("Successfully sent email", { toastId: "success1" });
+    } catch (err) {
+      toast.error("Something went wrong", { toastId: "error1" });
+    }
+  };
+
   return (
-    <Stack sx={{ minHeight: "100vh" }}>
-      <Typography className="mt-[150px]" color="error">
-        Temdeg orsn baih ystoi
-      </Typography>
-    </Stack>
+    <div>
+      <button className="border p-2" onClick={sendEmail}>
+        Email sender
+      </button>
+      <ToastContainer />
+    </div>
   );
 }
