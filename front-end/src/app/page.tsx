ebd104ca953e964451279cb58dbeb3ee3648a1f0
shagai-rgb <dash.altanshagai48@gmail.com>
2024-10-23 13:02:30 +0800

    asd (#105)
    Co-authored-by: 24LU1697 24LU1697 <24LU1697@G302-CC0111.local>

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -3,7 +3,12 @@ import OTP from "@/components/OTP";
 import { Stack, Typography } from "@mui/material";
 import NewPAss from "../components/NewPass";
 import Loading from "../components/Loading";
+import { HomeComp } from "@/components/home/HomeComp";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Stack sx={{ minHeight: "100vh", display: "flex", direction: "column" }}>
+      <HomeComp />
+    </Stack>
+  );
 }
